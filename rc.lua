48290eee71a8435033dc9e6a676fdf3e33bfd8e3
junk-e <47948104+junk-e@users.noreply.github.com>
2020-04-12 03:22:09 +0900

    [MOD] Add variables

--- rc.lua
+++ rc.lua
@@ -40,20 +40,13 @@ end
 -- }}}
 
 -- {{{ Variable definitions
+local modkey = "Mod4"
+local terminal = "termite"
+local editor   = os.getenv("EDITOR") or "vi"
+local editor_cmd = terminal .. " -e " .. editor
+
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
-
--- This is used later as the default terminal and editor to run.
-terminal = "xterm"
-editor = os.getenv("EDITOR") or "nano"
-editor_cmd = terminal .. " -e " .. editor
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
