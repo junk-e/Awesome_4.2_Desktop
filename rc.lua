6b46a79c1ea67573c41094ddfcc5728a715e1185
junk-e <47948104+junk-e@users.noreply.github.com>
2020-04-12 03:52:36 +0900

    [FEAT] Add Google-chrome rule

--- rc.lua
+++ rc.lua
@@ -477,9 +477,10 @@ awful.rules.rules = {
       }, properties = { titlebars_enabled = false }
     },
 
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
+    -- Set Google-chrome to always map on the tag 2 on screen 1.
+    { rule = { class = "Google-chrome" },
+        properties = { screen = 1, tag = awful.screen.focused().tags[2] }
+    },
 }
 -- }}}
 
